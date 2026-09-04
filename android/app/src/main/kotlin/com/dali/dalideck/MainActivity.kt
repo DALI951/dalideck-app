e7bc39296812ef5a1f7dfbb0e0fc2c92aa05e017
DALI951 <dali951@users.noreply.github.com>
2026-09-04 16:41:41 +0100

    fix: MainActivity now uses FlutterFragmentActivity so biometric prompt works
    - MainActivity.kt: FlutterActivity -> FlutterFragmentActivity (required by local_auth to host the BiometricPrompt fragment)
    - build.yml: new CI step re-patches MainActivity.kt to FlutterFragmentActivity after flutter create regenerates android/
    - pubspec.yaml: bump to 0.3.9+21

--- android/app/src/main/kotlin/com/dali/dalideck/MainActivity.kt
+++ android/app/src/main/kotlin/com/dali/dalideck/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.dali.dalideck
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
